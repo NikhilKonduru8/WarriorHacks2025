ed39e75e176e3f72901447f2a7ddf6047d34d56a
NikhilKonduru8 <nikhilkonduru8@gmail.com>
2025-04-21 11:02:03 -0700

    Update dbuzzer.py

--- MainCode/dbuzzer.py
+++ MainCode/dbuzzer.py
@@ -1,63 +1,66 @@
-import os
-import time
-import playsound
-import speech_recognition as sr
-from gtts import gTTS
-import google.generativeai as genai
-#from elevenlabs import play
-import random as rand
-import pyaudio
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ' ': ' '
+}
+import speech_recognition as sr 
 
 
-def speak (text):
-    tts = gTTS(text=text, lang="en")
-    filename = "voice.mp3"
-    tts.save(filename)
-    playsound.playsound(filename)
+r = sr.Recognizer()
+
+
+with sr.Microphone() as source:
+    print("Speak something...")
+    audio = r.listen(source)
+
+    try:
+       
+        said = r.recognize_google(audio)
+        print("You said:", said)
+    except Exception as e:
+        print("Error recognizing speech:", str(e))
+
+def text_to_morse_code(text):
+    
+    text = text.upper()
+    
+    morse_code = []
+    
+    for char in text:
+        if char in morse_code_dict:
+            morse_code.append(morse_code_dict[char])
+        else:
+            morse_code.append(char)  
+    return ' '.join(morse_code)
+
+
+input_text = said
+morse_code_result = text_to_morse_code(input_text)
+
+print("Morse Code:")
+print(morse_code_result)
 
 
 
-print("-------------------------------")
-print("What can I assist you with")
-print("-------------------------------")
-speak("What can I assist you with")
+from gpiozero import Buzzer 
 
-loop_options = ['Anything Else','Any other questions?','Need more help?','Anything else I can do?']
+from time import sleep
 
+buzzer = Buzzer(17) 
 
-
-
-while True:
-
-
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-        said = ""
-
-        try:
-            said = r.recognize_google(audio)
-        except Exception as e:
-            print("Exception: " + str(e))
-
-
-    genai.configure(api_key="AIzaSyBO5YGrnaTQ3YZjX46O1yJQ5ns4cam8swo")
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(said)
-
-    response = response.text.replace("*"," ")
-
-    time.sleep(2)
-    print("----------------------------------")
-    print("Prompt: "+ said)
-    print("----------------------------------")
-
-    print(response)
-    speak(response)
-    time.sleep(1.5)
-
-    choice = rand.choice(loop_options)
-    print("-------------------------------")
-    print(choice)
-    print("-------------------------------")
-    speak(choice)
+for i in morse_code_result:
+    if i == "-":
+        buzzer.on()
+        sleep(1.25)
+        buzzer.off()
+    if i == ".":
+        buzzer.on()
+        sleep(0.5)
+        buzzer.off()
+    if i == " ":
+        sleep(1.5)
+    sleep(0.5)
